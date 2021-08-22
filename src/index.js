6766d653355d54381218806a1f7303581d7983ad
Richard Huang <flyerhzm@gmail.com>
2021-08-22 13:54:13 +0800

    list snippets

--- src/index.js
+++ src/index.js
@@ -1,28 +1,45 @@
 const {Command, flags} = require('@oclif/command')
-const fs = require('fs')
 const { promisify } = require('util');
-const statAsync = promisify(fs.writeFile)
 const path = require('path')
-const { exec } = require('child_process')
+const glob = require("glob");
+const fs = require('fs')
+const stat = promisify(fs.stat)
+const exec = promisify(require('child_process').exec)
+const Synvert = require("synvert-core")
 
 class SynvertCommand extends Command {
   async run() {
     const {flags} = this.parse(SynvertCommand)
     if (flags.sync) {
-      await this.sync()
+      return await this.sync()
+    }
+    if (flags.list) {
+      return this.list()
     }
   }
 
   async sync() {
     const snippetsHome = this.snippetsHome()
     try {
-      await statAsync(snippetsHome)
+      await stat(snippetsHome)
       process.chdir(snippetsHome)
       await exec('git checkout .; git pull --rebase')
     } catch (err) {
-      await exec(`git clone https://github.com/xinminlabs/synvert-snippets.git ${snippetsHome}`)
+      await exec(`git clone https://github.com/xinminlabs/synvert-snippets-javascript.git ${snippetsHome}`)
     }
     this.log('snippets are synced')
+  }
+
+  list() {
+    const snippetsHome = this.snippetsHome()
+    glob.sync(path.join(snippetsHome, 'lib/**/*.js')).forEach(filePath => eval(fs.readFileSync(filePath, 'utf-8')))
+    const rewriters = Synvert.Rewriter.rewriters
+    Object.keys(rewriters).forEach(group => {
+      console.log(group)
+      Object.keys(rewriters[group]).forEach(name => {
+        console.log(`    ${name}`)
+      })
+    })
   }
 
   snippetsHome() {
@@ -38,6 +55,7 @@ SynvertCommand.flags = {
   // add --help flag to show CLI version
   help: flags.help({char: 'h'}),
   sync: flags.boolean({ description: 'sync snippets'}),
+  list: flags.boolean({ char: 'l', description: 'list snippets'}),
 }
 
 module.exports = SynvertCommand
