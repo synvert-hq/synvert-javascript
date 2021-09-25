69d4fd52547e3f7c2480ab04b220d3a53e484a81
Awesome Code <team@xinminlabs.com>
2021-09-25 07:20:38 +0000

    Auto corrected by following Format Javascript Code

--- src/index.js
+++ src/index.js
@@ -50,8 +50,8 @@ class SynvertCommand extends Command {
     const response = await fetch("https://registry.npmjs.org/synvert-core/latest");
     const json = await response.json();
     if (compareVersions(json.version, Synvert.version, ">")) {
-      console.log(`synvert-core is updated, installing synvert-core ${json.version}`)
-      await exec("npm install synvert-core")
+      console.log(`synvert-core is updated, installing synvert-core ${json.version}`);
+      await exec("npm install synvert-core");
     }
   }
 
